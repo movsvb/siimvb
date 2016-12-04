931a04b43c6edc553a864e5f7a1606ff7c4a8c72
movsvb <dineroextra92@hotmail.com>
2016-12-04 14:47:20 -0600

    se agrego new a la instancia s de saludo

--- prueba/prueba/Form1.vb
+++ prueba/prueba/Form1.vb
@@ -1,10 +1,11 @@
 ﻿Public Class Form1
+    Dim s As New Saludo
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim s As Saludo
-        TextBox1.Text = s.hola()
+        TextBox1.Text = s.hola
     End Sub
 End Class
